b6d1db2958eaf4e4226960a8264c6dfadb4b2276
fivetran-reneeli <renee.li@fivetran.com>
2023-07-05 18:59:30 -0500

    docs

--- macros/get_ad_group_history_columns.sql
+++ macros/get_ad_group_history_columns.sql
@@ -7,11 +7,11 @@
     {"name": "end_time", "datatype": dbt.type_timestamp()},
     {"name": "id", "datatype": dbt.type_string()},
     {"name": "name", "datatype": dbt.type_string()},
-    {"name": "pacing_delivery_type", "datetype": dbt.type_string()},
-    {"name": "placement_group", "datetype": dbt.type_string()},
+    {"name": "pacing_delivery_type", "datatype": dbt.type_string()},
+    {"name": "placement_group", "datatype": dbt.type_string()},
     {"name": "start_time", "datatype": dbt.type_timestamp()},
     {"name": "status", "datatype": dbt.type_string()},
-    {"name": "summary_status", "datetype": dbt.type_string()}
+    {"name": "summary_status", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
