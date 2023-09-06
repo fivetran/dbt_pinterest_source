eed902a039f4f2f03ccf707cfc0c7d5f1238eada
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-09-06 15:03:26 -0500

    updates

--- models/tmp/stg_pinterest_ads__keyword_report_tmp.sql
+++ models/tmp/stg_pinterest_ads__keyword_report_tmp.sql
@@ -3,11 +3,11 @@
 {{
     fivetran_utils.union_data(
         table_identifier='keyword_report', 
-        database_variable='pinterest_ads_database', 
-        schema_variable='pinterest_ads_schema', 
+        database_variable='pinterest_database', 
+        schema_variable='pinterest_schema', 
         default_database=target.database,
         default_schema='pinterest_ads',
-        default_variable='keyword_report_source',
+        default_variable='keyword_report',
         union_schema_variable='pinterest_ads_union_schemas',
         union_database_variable='pinterest_ads_union_databases'
     )
--- models/tmp/stg_pinterest_ads__pin_promotion_report_tmp.sql
+++ models/tmp/stg_pinterest_ads__pin_promotion_report_tmp.sql
@@ -3,11 +3,11 @@
 {{
     fivetran_utils.union_data(
         table_identifier='pin_promotion_report', 
-        database_variable='pinterest_ads_database', 
-        schema_variable='pinterest_ads_schema', 
+        database_variable='pinterest_database', 
+        schema_variable='pinterest_schema', 
         default_database=target.database,
         default_schema='pinterest_ads',
-        default_variable='pin_promotion_report_source',
+        default_variable='pin_promotion_report',
         union_schema_variable='pinterest_ads_union_schemas',
         union_database_variable='pinterest_ads_union_databases'
     )
