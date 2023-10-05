2a9c73489f51b4c1235506ef046fe8dedffeece6
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-10-05 12:44:48 -0500

    update naming

--- models/tmp/stg_pinterest_ads__ad_group_history_tmp.sql
+++ models/tmp/stg_pinterest_ads__ad_group_history_tmp.sql
@@ -3,8 +3,8 @@
 {{
     fivetran_utils.union_data(
         table_identifier='ad_group_history', 
-        database_variable='pinterest_database', 
-        schema_variable='pinterest_schema', 
+        database_variable='pinterest_ads_database', 
+        schema_variable='pinterest_ads_schema', 
         default_database=target.database,
         default_schema='pinterest_ads',
         default_variable='ad_group_history',
--- models/tmp/stg_pinterest_ads__keyword_history_tmp.sql
+++ models/tmp/stg_pinterest_ads__keyword_history_tmp.sql
@@ -3,8 +3,8 @@
 {{
     fivetran_utils.union_data(
         table_identifier='keyword_history', 
-        database_variable='pinterest_database', 
-        schema_variable='pinterest_schema', 
+        database_variable='pinterest_ads_database', 
+        schema_variable='pinterest_ads_schema', 
         default_database=target.database,
         default_schema='pinterest_ads',
         default_variable='keyword_history',
