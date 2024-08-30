055491c7eaf9f8a58a11694b140e65886feb4b2c
Renee Li <renee.li@fivetran.com>
2024-08-30 14:04:58 -0400

    fix macro ref

--- macros/get_keyword_report_columns.sql
+++ macros/get_keyword_report_columns.sql
@@ -21,7 +21,7 @@
 ] %}
 
 {# Make backwards compatible in case users were bringing in conversion metrics via passthrough columns prior to us adding them by default #}
-{{ pinterest_ads_fill_pass_through_columns(base_columns=columns, pass_through_fields=var('pinterest__keyword_report_passthrough_metrics'), except_fields=['total_conversions', "total_conversions_quantity", "total_conversions_value_in_micro_dollar"]) }}
+{{ pinterest_ads_add_passthrough_columns(base_columns=columns, pass_through_fields=var('pinterest__keyword_report_passthrough_metrics'), except_fields=['total_conversions', "total_conversions_quantity", "total_conversions_value_in_micro_dollar"]) }}
 
 {{ return(columns) }}
 
