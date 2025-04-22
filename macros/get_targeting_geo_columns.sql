ba167ec91030c4806cbf8f524d7efe890afd6dc1
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-21 22:03:57 -0500

    update get_col macros

--- macros/get_targeting_geo_columns.sql
+++ macros/get_targeting_geo_columns.sql
@@ -6,9 +6,6 @@
     {"name": "_fivetran_synced", "datatype": "dbt.type_timestamp()"}
 ] %}
 
-{# Make backwards compatible in case users were bringing in conversion metrics via passthrough columns prior to us adding them by default #}
-{{ pinterest_ads_add_passthrough_columns(base_columns=columns, pass_through_fields=var('pinterest__targeting_geo_passthrough_metrics'), except_fields=['total_conversions', "total_conversions_quantity", "total_conversions_value_in_micro_dollar"]) }}
-
 {{ return(columns) }}
 
 {% endmacro %}
--- macros/get_targeting_geo_region_columns.sql
+++ macros/get_targeting_geo_region_columns.sql
@@ -7,9 +7,6 @@
     {"name": "region_name", "datatype": "dbt.type_string()"}
 ] %}
 
-{# Make backwards compatible in case users were bringing in conversion metrics via passthrough columns prior to us adding them by default #}
-{{ pinterest_ads_add_passthrough_columns(base_columns=columns, pass_through_fields=var('pinterest__targeting_geo_region_passthrough_metrics'), except_fields=['total_conversions', "total_conversions_quantity", "total_conversions_value_in_micro_dollar"]) }}
-
 {{ return(columns) }}
 
 {% endmacro %}
