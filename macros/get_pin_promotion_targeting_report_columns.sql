584734ec4dea2c386fc107aa7474b34bbea14387
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-23 13:29:43 -0500

    Update macro

--- macros/get_pin_promotion_targeting_report_columns.sql
+++ macros/get_pin_promotion_targeting_report_columns.sql
@@ -19,8 +19,7 @@
     {"name": "total_conversions_value_in_micro_dollar", "datatype": dbt.type_int()}
 ] %}
 
-{# Make backwards compatible in case users were bringing in conversion metrics via passthrough columns prior to us adding them by default #}
-{{ pinterest_ads_add_passthrough_columns(base_columns=columns, pass_through_fields=var('pinterest__pin_promotion_targeting_report_passthrough_metrics'), except_fields=['total_conversions', "total_conversions_quantity", "total_conversions_value_in_micro_dollar"]) }}
+{{ pinterest_ads_add_passthrough_columns(base_columns=columns, pass_through_fields=var('pinterest__pin_promotion_targeting_report_passthrough_metrics')) }}
 
 {{ return(columns) }}
 
